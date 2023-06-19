650e40988222539d40d8c8ef6e6cf722358a6ef8
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2023-06-19 16:18:55 +0000

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿void Print(string text);
+{
+    Console.WriteLine(text);
+}
+
+int GetNumber()
+{
+    int number = Convert.ToInt32(ReadLine());
+    return number;
+}
+
+bool GetPolindr(int number);
+{
+    if(numberr>100 && number<1000)
+    {
+        if(number%10 == numberr/100)
+        return true;
+        else 
+        return false;
+    }
+    if(number>1000 && number<10000)
+    {
+        if(number%10 == number/1000 && number/10%10 == number/100%10)
+        return true;
+        else
+        return false;
+    }
+}
+Print("Inter number: ");
+GetNumber();
+GetPolindr(GetNumber);
