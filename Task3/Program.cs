df08b602b1409722725c3323ed6db599b4b33f3c
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2023-06-20 07:41:04 +0000

    Add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿void Print(string text)
+{
+    Console.WriteLine(text);
+}
+
+double GetNumber()
+{
+    double number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void GetCubes(double number)
+{
+    for(double i=1; i<=number; i++)
+    {
+        double cubes = Math.Pow(i, 3);
+        Console.Write($"{cubes}, ");
+    }
+}
+
+Print("Enter number");
+double number = GetNumber();
+GetCubes(number);
