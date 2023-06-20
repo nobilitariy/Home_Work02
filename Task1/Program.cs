7ea431e6049def978e0023822465ed0467abc968
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2023-06-20 07:19:06 +0000

    Add Task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,40 +2,27 @@
 {
     Console.WriteLine(text);
 }
-/*
+
 double GetNumber()
 {
     double number = Convert.ToInt32(Console.ReadLine());
     return number;
 }
 
-bool GetPolindr(double number)
-{
-    if(number > 99 && number < 1000)
-    {
-        if(number % 10 == number / 100) return true;
-        else return false;      
-    } 
-    else if(number > 999 && number < 10000)
-    {
-        if(number % 10 == number / 1000 && number / 10 % 10 == number / 100 % 10) return true;
-        else return false;
-    }
-    return false;
-}
-*/
-
-double GetPolindr(double number)
+double ReversеNumber(double number)
 {
     double res = 0;
     while(number > 0)
     {
-        number = number % 10;
-        res = res * 10 + number;
-        number = number / 10;       
-    }  
+        double digit = number % 10;
+        res = res * 10 + digit;
+        number = number / 10;     
+    }
+    return res;
 }
-//Print("Inter number: ");
-double result = GetPolindr(565);
+
+Print("Inter number: ");
+double num = GetNumber();
+double result = ReversеNumber(num);
 Print($"{result}");
 
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿void Print(string text)
+{
+    Console.WriteLine(text);
+}
+
+double GetNumber()
+{
+    double num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+double CalculDistans(double x1, double y1, double z1,
+                     double x2, double y2, double z2)
+{
+    double DistanceFirst = Math.Sqrt((x2-x1)*(x2-x1)+(z2-z1)*(z2-z1));
+    double DistanceFinal = Math.Sqrt(DistanceFirst*DistanceFirst+(y2-y1)*(y2-y1));
+    return DistanceFinal;
+
+}
+
+Print("Еnter coordinates of 2 points in 3D space");
+double x1 = GetNumber();
+double y1 = GetNumber();
+double z1 = GetNumber();
+double x2 = GetNumber();
+double y2 = GetNumber();
+double z2 = GetNumber();
+double result = CalculDistans(x1, y1, z1, x2, y2, z2);
+Print($"Distance between two points in 3D space = {result}");
